e6a6dcdfc3a10d30ca9cadf05c3d111b3ac0f97c
samplenehgithub <106725123+samplenehgithub@users.noreply.github.com>
2022-07-22 01:44:53 +0530

    Add files via upload

---
+++ BookRecommendationApi/src/test/java/com/recommendation/RecomendationApi/RecomendationApiApplicationTests.java
@@ -0,0 +1,10 @@
+package com.recommendation.RecomendationApi;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class RecomendationApiApplicationTests {
+	
+		
+	}
